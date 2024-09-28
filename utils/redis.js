3d77e9aeb0647bc3820e1899b655b7a0f8e887ca
loxiasblack <iliaschokairi@gmail.com>
2024-09-28 11:51:14 +0100

    create my first class in utils redis

--- utils/redis.js
+++ utils/redis.js
@@ -1,31 +1,40 @@
 import { createClient } from 'redis';
+import { promisify } from 'util';
 
 class RedisClient {
-    constructor() {
-        this.client = createClient()
-         .on('error', (error) => {
-            console.log(`Redis client not connected to the server: ${error}`);
-         })
-        
-         .on('connect', () => {
-            console.log('Redis client connected to the server');
-         });
-    }   
+  constructor() {
+    this.client = createClient()
+      .on('error', (error) => {
+        console.log(`Redis client not connected to the server: ${error}`);
+        this.connected = false;
+        this.client.quit();
+      });
 
-    async get(key) {
-        // return the value of the key
-        return this.client.get(key);
-    }
+    this.getAsync = promisify(this.client.get).bind(this.client);
+    this.SetAsync = promisify(this.client.set).bind(this.client);
+    this.delAsync = promisify(this.client.get).bind(this.client);
+    this.connected = true;
+  }
 
-    async set(key, value, duration) {
-        // set the value of the key with an expiration time
-        return this.client.set(key, value, 'EX', duration);
-    }
+  isAlive() {
+    return this.connected;
+  }
 
-    async del(key) {
-        // delete the key
-        return this.client.del(key);
-    }
+  async get(key) {
+    // return the value of the key
+    const value = await this.getAsync(key);
+    return value;
+  }
+
+  async set(key, value, duration) {
+    // set the value of the key with an expiration time
+    await this.SetAsync(key, value, 'EX', duration);
+  }
+
+  async del(key) {
+    // delete the key
+    await this.delAsync(key);
+  }
 }
 
 const redisClient = new RedisClient();
