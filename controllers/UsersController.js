f8f1bea9cd8c8203bdd9bf1661c7165b1f6f133f
loxiasblack <iliaschokairi@gmail.com>
2024-09-30 16:43:37 +0100

    try this time

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -6,22 +6,22 @@ class UsersController {
     try {
       const { email, password } = req.body;
       if (!email) {
-        res.status(400).send({ error: 'Missing email' });
+        return res.status(400).send({ error: 'Missing email' });
       }
       if (!password) {
-        res.status(400).send({ error: 'Missing password' });
+        return res.status(400).send({ error: 'Missing password' });
       }
       const user = await dbClient.getUser(email);
       if (user) {
-        res.status(400).send({ error: 'Already exist' });
+        return res.status(400).send({ error: 'Already exist' });
       }
       const hash = crypto.createHash('sha1');
       hash.update(password);
       const hashedPassword = hash.digest('hex');
       const newUser = await dbClient.addUser({ email, password: hashedPassword });
-      res.status(201).send({ id: newUser.insertedId, email });
+      return res.status(201).send({ id: newUser.insertedId, email });
     } catch (error) {
-      console.log(`error: ${error}`);
+      return res.status(500).send({ error });
     }
   }
 }
